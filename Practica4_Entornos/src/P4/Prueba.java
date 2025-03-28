17d2fe09dbae34c9ac65ca29ede43bfb52a30e25
Jorge <Jorge@DESKTOP-2IDB345>
2025-03-28 17:27:17 +0100

    Practica4_Entornos V1

---
+++ Practica4_Entornos/src/P4/Prueba.java
@@ -0,0 +1,17 @@
+package P4;
+
+import java.util.Scanner;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Escribe un número");
+		int num1 = sc.nextInt();
+		System.out.println("Escribe por cual lo vas a sumar");
+		int num2 = sc.nextInt();
+		System.out.println(num1+" + "+num2+" = "+(num1+num2));
+	}
+
+}
