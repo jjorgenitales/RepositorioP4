823bfe63a4f4f6ea38627b1bd07ea27541b76a93
Jorge <Jorge@DESKTOP-2IDB345>
2025-03-29 17:31:27 +0100

    Practica4_Entornos V1.2

--- Practica4_Entornos/src/P4/Resta.java
+++ Practica4_Entornos/src/P4/Resta.java
@@ -7,13 +7,13 @@ public class Resta {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Escribe un número");
+		System.out.println("Escribe un número:");
 		int num1 = sc.nextInt();
-		System.out.println("Escribe por cual lo vas a sumar");
+		System.out.println("Escribe por cual lo vas a sumar:");
 		int num2 = sc.nextInt();
 		int suma = num1+num2;
 		System.out.println(num1+" + "+num2+" = "+suma);
-		System.out.println("Escribe por cual lo vas a restar");
+		System.out.println("Escribe por cual lo vas a restar:");
 		int num3 = sc.nextInt();
 		int resta = suma-num3;
 		System.out.println(suma+" - "+num3+" = "+resta);
