3b64f2867aedd60249a6707898fee8006453c9fd
Jorge <Jorge@DESKTOP-2IDB345>
2025-03-29 17:26:17 +0100

    Practica4_Entornos V1.2

--- Practica4_Entornos/src/P4/Resta.java
+++ Practica4_Entornos/src/P4/Resta.java
@@ -9,9 +9,14 @@ public class Resta {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Escribe un número");
 		int num1 = sc.nextInt();
-		System.out.println("Escribe por cual lo vas a restar");
+		System.out.println("Escribe por cual lo vas a sumar");
 		int num2 = sc.nextInt();
-		System.out.println(num1+" - "+num2+" = "+(num1-num2));
+		int suma = num1+num2;
+		System.out.println(num1+" + "+num2+" = "+suma);
+		System.out.println("Escribe por cual lo vas a restar");
+		int num3 = sc.nextInt();
+		int resta = suma-num3;
+		System.out.println(suma+" - "+num3+" = "+resta);
 	}
 
 }
