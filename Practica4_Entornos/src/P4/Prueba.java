3b21ecc9d0a69f5ca0ad25367954a45a1f99a842
Jorge <Jorge@DESKTOP-2IDB345>
2025-03-31 13:56:59 +0200

    Clase Prueba, cambio multiplicación

--- Practica4_Entornos/src/P4/Prueba.java
+++ Practica4_Entornos/src/P4/Prueba.java
@@ -9,9 +9,9 @@ public class Prueba {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Escribe un número");
 		int num1 = sc.nextInt();
-		System.out.println("Escribe por cual lo vas a sumar");
+		System.out.println("Escribe por cual lo vas a multiplicar");
 		int num2 = sc.nextInt();
-		System.out.println(num1+" + "+num2+" = "+(num1+num2));
+		System.out.println(num1+" * "+num2+" = "+(num1*num2));
 	}
 
 }
